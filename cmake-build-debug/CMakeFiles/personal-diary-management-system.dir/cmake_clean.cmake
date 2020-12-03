b8f2bdef7a385a4310e26078f1567c95a7491cfa
Ali Farooq <alifarooq122@gmail.com>
2020-12-03 21:00:46 +0500

    first commit of personal diary
    this contain basic landing page

---
+++ cmake-build-debug/CMakeFiles/personal-diary-management-system.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/personal-diary-management-system.dir/main.c.obj"
+  "personal-diary-management-system.exe"
+  "personal-diary-management-system.exe.manifest"
+  "personal-diary-management-system.lib"
+  "personal-diary-management-system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/personal-diary-management-system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
